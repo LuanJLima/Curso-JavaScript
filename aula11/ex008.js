ea1d1d06ce4fc59e5c5bd7f349f3c4e2a254ed1e
LuanJLima <luan.jonatas@hotmail.com>
2024-06-18 12:33:22 -0300

    .

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if (vel>60) {
+    console.log(`Voce passou a velocidade permitida. MULTADO!`)
+}else {
+    console.log('Voce esta dentro do limite de velocidade, siga com Seguraça')
+}
+console.log('Dirija sempre com o cinto de segurança')
